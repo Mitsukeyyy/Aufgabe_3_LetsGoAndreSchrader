0cf2218e02900fd5eb3eb40f2adcc6ad4713f42f
Andre Schrader <schradera24@gmail.com>
2025-06-07 23:19:40 +0200

    fixed Highscore Script

--- Assets/+++Workdata+++/Scripts/Highscore.cs
+++ Assets/+++Workdata+++/Scripts/Highscore.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class Highscore : MonoBehaviour
 {
     [SerializeField] private int[] highscore =  new int[5];
-    //[SerializeField] private int highscore2 = 9999;
     [SerializeField] TextMeshProUGUI[] highscoreText;
-    //public TextMeshProUGUI HighscoreText2;
 
-    [SerializeField] UI_Manager uimanager; 
+    [SerializeField] UI_Manager uimanager;
+    private bool updated = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +16,7 @@ public class Highscore : MonoBehaviour
         {
             highscore[i] = PlayerPrefs.GetInt("Highscore" + i, 9999);
         }
-        
+        updated = false;
         UpdateList();
     }
 
@@ -29,37 +29,39 @@ public class Highscore : MonoBehaviour
             PlayerPrefs.Save();
             Debug.Log("PlayerPrefs cleared.");
         }
-
-        UpdateHighscore(uimanager.score);
+        if (!updated){
+            UpdateHighscore(uimanager.score);
+        }
         
     }
 
     private void UpdateHighscore(int newScore)
     {
-            if (newScore >= highscore[highscore.Length - 1])
-                return;
-            
-            for (int i = 0; i < highscore.Length; i++)
-            {
-                if (highscore[i] == newScore)
-                    return;
-            }
-            highscore[highscore.Length - 1] = newScore;
-            System.Array.Sort(highscore);
-            
-            for (int i = 0; i < highscoreText.Length; i++)
-            {
-                PlayerPrefs.SetInt("Highscore" + i, highscore[i]);
-            }
-            PlayerPrefs.Save();
-            UpdateList();
+        if (newScore >= highscore[highscore.Length - 1])
+            return;
+        highscore[highscore.Length - 1] = newScore;
+        System.Array.Sort(highscore);
+
+        for (int i = 0; i < highscore.Length; i++)
+        {
+            PlayerPrefs.SetInt("Highscore" + i, highscore[i]);
+        }
+        PlayerPrefs.Save();
+        UpdateList();
+        updated = true;
              
     }
     private void UpdateList()
     {
-        for (int i = 0; i < highscore.Length && i < highscoreText.Length; i++)
+        for (int i = 0; i < highscore.Length; i++)
         {
-                highscoreText[i].text = highscore[i].ToString();
+            if (highscore[i] == 9999){
+                highscoreText[i].text = "";
+            }
+            else
+            {
+                highscoreText[i].text = (i + 1) + ": " + highscore[i].ToString();
+            }
         }
     }
   
